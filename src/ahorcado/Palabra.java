0d70d133fee841b9e2589bec56b8c2d9c6e2aba7
tiquitaca <Andres.sm123@gmail.com>
2015-10-20 13:24:48 -0600

    Arreglos (re subido)
    Primer commit

---
+++ src/ahorcado/Palabra.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ahorcado;
+
+/**
+ *
+ * @author Admin
+ */
+public class Palabra {
+    
+    public String categoria[]  = {"ANIMALES", "PLANTAS"};
+    public String animales[] = {"AGUILA", "ABEJA", "ALACRAN", "ALCATRAZ", "ALCE", "ANTILOPE", "AVESTRUZ","BALLENA","BISONTE", "BOROCHI", "BUFALO","CACATUA","CALAMAR","CAMELLO","DROMEDARIO","DELFIN","ERIZO", "ESCUERZO","ELEFANTE", "FLAMENCO", "FOCA","GACELA","GAVIOTA","GAVILAN","HIENA", "HUEMUL","IGUANA", "JABIRU", "JAGUARUNDI","JIRAFA", "KOALA","KRILL","LEMUR","LUBINA", "MAPACHE", "MOFETA", "NUTRIA", "ORNITORRINCO", "OSO", "PANTERA", "PAPAGAYO", "PELICANO","PIHUEL", "QUOKKA", "QUAGGA","RINOCERONTE", "SALAMANDRA", "SURICATA", "TOPO", "TORTUGA", "TUCAN","VACA", "VIBORA", "VINCHUCA", "WALABI", "XOLOESCUINTLE", "YAK", "ZARIGUEYA"};
+    public String plantas[] = {"ALMENDRO", "AMAPOLA", "AGAVE", "BALSAMO", "BUDELIA", "CARDO", "CARDOSANTO", "DRAGONARIA", "EQUINOCACTUS", "GANDUL", "GRANADO", "HINOJO", "JAZMIN", "LAVANDA", "LINO", "MANZANILLA", "MARGARITA", "OXALIS", "PAPIRO", "PETUNIA", "ROMERO", "TOMILLO", "TULIPERO", "VIBORERA", "ALAMO", "ARCE", "CALANIT"};
+}
+
