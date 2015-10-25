066592d9f367b1d11a1dcfbae8b3f0ec9d2d4e2f
tiquitaca <Andres.sm123@gmail.com>
2015-10-25 05:41:45 -0600

    Modificación VIII

--- src/ahorcado/Ahorcado.java
+++ src/ahorcado/Ahorcado.java
@@ -10,77 +10,93 @@ import java.util.Scanner;
 
 /**
  *
- * @author Admin
+ * @author Andrés Matta
  */
 public class Ahorcado {
-
     /**
      * @param args the command line arguments
      * 
      */
     public static void main(String[] args) {
-    
+        //Para la lectura de datos.
         Scanner sc = new Scanner(System.in);
+        //Variables traidas de la clase Palabra.
         char letra=0;
         int intentos= 0;
-        String categoria= JOptionPane.showInputDialog("Elija la categoria: A.Animales B.Plantas");
+        String categoria="";
         String aleatoria="";
-      
+       //Variables propias de la clase main.
         String secreta="";
         int i = 0;
         int posicion1;
         int posicion2;
+        //Variables utilizadas en los menus.
+        String menu;
+        int r=0;
         String reintentar;
-        
+        //Instancia de la clase Palabra.
         Palabra p1 = new Palabra(intentos, categoria, aleatoria, letra);
-        
-        while(p1.aleatoria.length()>i){
+        //Ciclo que permite volver a reiniciar el juego.
+        while(r==0){
+       //If que genera el menu para jugar o salir.
+        menu=JOptionPane.showInputDialog("A.Jugar B.Salir");
+         if(menu.equalsIgnoreCase("B")){
+            System.exit(intentos);
+        }
+        //igenera la cadena random para la variable aleatoria, por medio de los métodos de la clase Palabra.
+        p1.categoria= JOptionPane.showInputDialog("Elija la categoria: A.Animales B.Plantas");
+        if(p1.categoria.equalsIgnoreCase("A")){
+            p1.aleatoria=p1.animales[p1.PosicionAnimales()];
+        }else{
+            p1.aleatoria=p1.plantas[p1.PosicionPlantas()];
+        }
+         //genera una cadena llena de "*" de la misma longitud que la variable aleatoria.
+       while(p1.aleatoria.length()>i){
           secreta= secreta + "*";  
           i=i+1;}
-        
+        //Revela la cantidad de digitos de la palabra.
         JOptionPane.showMessageDialog(null, "el número de caracteres de la palabra es: " + p1.aleatoria.length());
         JOptionPane.showMessageDialog(null, p1.aleatoria);
         System.out.println("¡Adivina la palabra! \r" +  secreta);
-        
+        //Permite modificar una cadena de caracteres.
         StringBuffer secretaBd = new StringBuffer(secreta);
-        
+        //Ciclo del juego, con el contador de intentos.
                 while(p1.aleatoria.equals(secretaBd.toString())==false && intentos<11){
-                
-                    System.out.print("Ingrese una letra");
+                System.out.print("Ingrese una letra");
                     letra =sc.next().charAt(0);
-                   
+                   //Permite ingresar minúsculas.
                     if(Character.isLowerCase(letra))
                        letra=Character.toUpperCase(letra);
-                   
+                   //Busca el caracter en la palabra, si no esta devuelvo un int -1.
                     if(p1.aleatoria.indexOf(letra)==-1){
                     JOptionPane.showMessageDialog(null, "Incorrecto");
                     intentos=intentos+1;
-                   }
+                   }//Si lo encuentra, retorna un valor entero >=0.
                     else if (p1.aleatoria.indexOf(letra)>=0){
                     posicion1= p1.aleatoria.indexOf(letra);
                     posicion2=p1.aleatoria.lastIndexOf(letra);
-                    
+                    //Modifica la cadena del StringBuffer.
                     secretaBd.setCharAt(posicion1, letra);
                     secretaBd.setCharAt(posicion2, letra);
-                    
+                    //Revela letras acertadas.
                     JOptionPane.showMessageDialog(null, "¡Correcto!" );
                     JOptionPane.showMessageDialog(null, secretaBd);
-                    
                     }
                     }
-        
+        //Informa al usuario que perdió si el contador llego 11, revela la palabra oculta.
         if (intentos>=11){
+                    System.out.print(p1.aleatoria);
                     JOptionPane.showMessageDialog(null, "Perdiste, la palabra era: " + p1.aleatoria);
-                    System.out.print(aleatoria);
-                    System.exit(0);
+        //Mensaje de felicitacion.            
         }else if(p1.aleatoria.equals(secretaBd.toString())){
-            JOptionPane.showMessageDialog(null, "¡Felicidades ganaste!");
-             System.out.print(aleatoria);
-                    System.exit(0);
+            JOptionPane.showMessageDialog(null, "¡Felicidades ganaste!");  
         }
-        
-       
+        //Opcion de reintentar.
+        reintentar=JOptionPane.showInputDialog("¿Desea volver a jugar? A.Sí B.No");
+        if(reintentar.equalsIgnoreCase("B"))
+            //Fin del programa.
+            System.exit(r);
         }
         }
-     
+        }
 
--- src/ahorcado/Palabra.java
+++ src/ahorcado/Palabra.java
@@ -5,87 +5,103 @@
  */
 package ahorcado;
 
-
-
-
 /**
  *
- * @author Admin
+ * @author Andrés Matta
  */
 public class Palabra {
-    
+    //Variable categoria que guardara un valor insetado por el usuario.
     public String categoria="";
-    public String[] animales={"AGUILA", "ABEJA", "ALCE", "ANTILOPE", "AVESTRUZ", "BALLENA", "BISONTE", "BOROCHI", "BUFALO", "CAMELLO", "DROMEDARIO", "DELFIN", "ERIZO", "ESCUERZO", "FLAMENCO", "FOCA", "GACELA", "GAVIOTA", "GAVILAN", "HIENA", "HUEMUL", "JABIRU", "JAGUARUNDI", "JIRAFA", "KOALA", "KRILL", "LEMUR", "LUBINA", "MAPACHE", "MOFETA", "NUTRIA", "OSO", "PANTERA", "PELICANO", "PIHUEL", "QUOKKA", "QUAGGA", "RINOCERONTE", "SURICATA", "TOPO", "TORTUGA", "TUCAN", "VACA", "VIBORA", "VINCHUCA", "WALABI", "XOLOESCUINTLE", "YAK", "ZARIGUEYA"};
-    public String[] plantas={"ALMENDRO", "AMAPOLA", "AGAVE", "BALSAMO", "BUDELIA", "CARDO", "CARDOSANTO", "EQUINOCACTUS", "GANDUL", "GRANADO", "HINOJO", "JAZMIN", "LINO", "OXALIS", "PAPIRO", "PETUNIA", "ROMERO", "TOMILLO", "TULIPERO", "VIBORERA", "ALAMO", "ARCE", "CALANIT"};
-    public char[] letra;
+    //Arreglo que guarda las palabras de la categoria animales.
+    public String[] animales;
+    //Arreglo que guarda las palabras de la categoria plantas.
+    public String[] plantas;
+    //Arreglo que alojara el carácter insertado por el usario.
+    public char[] letra; 
+    //Variable utilizada como contador
     private int intentos;
+    //Variable donde se alojara una palabra random de uno de los arreglos.
     String aleatoria;
-    
-    
+    /**
+     *Método constructor para los arreglos y variables.
+     * @param intentos
+     * @param categoria
+     * @param aleatoria
+     * @param letra
+     */
     public Palabra(int intentos, String categoria, String aleatoria, char letra) {
-    
+        this.plantas = new String[]{"ALMENDRO", "AMAPOLA", "AGAVE", "BALSAMO", "BUDELIA", "CARDO", "CARDOSANTO", "EQUINOCACTUS", "GANDUL", "GRANADO", "HINOJO", "JAZMIN", "LINO", "OXALIS", "PAPIRO", "PETUNIA", "ROMERO", "TOMILLO", "TULIPERO", "VIBORERA", "ALAMO", "ARCE", "CALANIT"};
+        this.animales = new String[]{"AGUILA", "ABEJA", "ALCE", "ANTILOPE", "AVESTRUZ", "BALLENA", "BISONTE", "BOROCHI", "BUFALO", "CAMELLO", "DROMEDARIO", "DELFIN", "ERIZO", "ESCUERZO", "FLAMENCO", "FOCA", "GACELA", "GAVIOTA", "GAVILAN", "HIENA", "HUEMUL", "JABIRU", "JAGUARUNDI", "JIRAFA", "KOALA", "KRILL", "LEMUR", "LUBINA", "MAPACHE", "MOFETA", "NUTRIA", "OSO", "PANTERA", "PELICANO", "PIHUEL", "QUOKKA", "QUAGGA", "RINOCERONTE", "SURICATA", "TOPO", "TORTUGA", "TUCAN", "VACA", "VIBORA", "VINCHUCA", "WALABI", "XOLOESCUINTLE", "YAK", "ZARIGUEYA"};
         this.letra = new char[0];
         this.categoria = categoria;
-        this.aleatoria= this.PalabraAleatoria(aleatoria);
-        this.plantas = new String[]{"ALMENDRO", "AMAPOLA", "AGAVE", "BALSAMO", "BUDELIA", "CARDO", "CARDOSANTO", "DRAGONARIA", "EQUINOCACTUS", "GANDUL", "GRANADO", "HINOJO", "JAZMIN", "LAVANDA", "LINO", "MANZANILLA", "MARGARITA", "OXALIS", "PAPIRO", "PETUNIA", "ROMERO", "TOMILLO", "TULIPERO", "VIBORERA", "ALAMO", "ARCE", "CALANIT"};
-        this.animales = new String[]{"AGUILA", "ABEJA", "ALACRAN", "ALCATRAZ", "ALCE", "ANTILOPE", "AVESTRUZ", "BALLENA", "BISONTE", "BOROCHI", "BUFALO", "CACATUA", "CALAMAR", "CAMELLO", "DROMEDARIO", "DELFIN", "ERIZO", "ESCUERZO", "ELEFANTE", "FLAMENCO", "FOCA", "GACELA", "GAVIOTA", "GAVILAN", "HIENA", "HUEMUL", "IGUANA", "JABIRU", "JAGUARUNDI", "JIRAFA", "KOALA", "KRILL", "LEMUR", "LUBINA", "MAPACHE", "MOFETA", "NUTRIA", "ORNITORRINCO", "OSO", "PANTERA", "PAPAGAYO", "PELICANO", "PIHUEL", "QUOKKA", "QUAGGA", "RINOCERONTE", "SALAMANDRA", "SURICATA", "TOPO", "TORTUGA", "TUCAN", "VACA", "VIBORA", "VINCHUCA", "WALABI", "XOLOESCUINTLE", "YAK", "ZARIGUEYA"};
-        this.intentos = intentos;
-        
+        this.aleatoria= aleatoria;
     }
+
+    /**
+     *Método estándar get para el arreglo letra.
+     * @return letra
+     */
     public char[] getLetra() {
         return letra;
     }
 
+    /**
+     *Método estándar set para el arreglo char letra.
+     * @param letra
+     */
     public void setLetra(char[] letra) {
         this.letra = letra;
     }
 
+    /**
+     *Método estándar get para la variable intentos.
+     * @return intentos.
+     */
     public int getIntentos() {
         return intentos;
     }
 
+    /**
+     *Método estándar set para la variable intentos.
+     * @param intentos
+     */
     public void setIntentos(int intentos) {
         this.intentos = intentos;
     }
 
+    /**
+     *Método estándar get para la variable aleatoria.
+     * @return aleatoria
+     */
     public String getAleatoria() {
         return aleatoria;
     }
 
+    /**
+     *Método estándar se para la variable aleatoria.
+     * @param aleatoria
+     */
     public void setAleatoria(String aleatoria) {
         this.aleatoria = aleatoria;
     }
 
-    
+    /**
+     *Método que obtiene un random apartir del arreglo animales.
+     * @return posicion
+     */
     public int PosicionAnimales(){
-        
         int posicion = (int)(Math.random() *animales.length);
-        return posicion;
-        
+        return posicion; 
     }
     
+    /**
+     *Método que obtiene un random apartir del arreglo de plantas.
+     * @return posicion 
+     */
     public int PosicionPlantas(){
-        
         int posicion = (int)(Math.random() *plantas.length);
         return posicion;
+    } 
     }
-    
-    
-    private String PalabraAleatoria(String aleatoria){
-        if ("A".equalsIgnoreCase(categoria)){
-            aleatoria = animales[PosicionAnimales()];
-        }   else{
-            aleatoria= plantas[PosicionPlantas()];
-        }
-        return aleatoria;
-    }   
-     
-    
-    @Override
-    public String toString() {
-        return "Palabra{" + "categoria=" + categoria + ", animales=" + animales + ", plantas=" + plantas + ", letra=" + letra + ", intentos=" + intentos + ", elegido=" + aleatoria + '}';
-    }
-    
-}
 
 
